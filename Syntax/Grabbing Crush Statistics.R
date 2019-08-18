c775b4a7d7e391cf0194411675208ee2d78590a7
Shane Smith <shane.d.smith80@gmail.com>
2019-08-19 00:34:58 +1000

    Update Grabbing Crush Statistics.R
    Added outputs and improved commenting in some places

--- Syntax/Grabbing Crush Statistics.R
+++ Syntax/Grabbing Crush Statistics.R
@@ -1,18 +1,37 @@
+# Grabbing Crush Statistics 
+# Inputs: 
+#    - None: draws data from the web. 
+# 
+# Outputs: 
+#    - Weekly crush statistics in XLSX, CSV and RDS formats, in a supplementary_data
+#      subfolder. 
+# 
+# Overview: 
+#    - This script takes the data in the 2018 and 2019 crush statistics and 
+#      outputs them in Excel format, also in CSV and RDS. 
+
 # ------------------------------------------------------------------------------
 ## Preliminaries ---- 
 ## Load Packages 
-library(readr) 
-library(glue) 
-library(magrittr) 
-library(openxlsx) 
-library(dplyr) 
-library(rvest) 
-library(stringr) 
+library(readr)    # Easier read/write from other formats 
+library(glue)     # Easier string concatenation 
+library(magrittr) # Piping functions for easier understanding 
+library(openxlsx) # For outputting to Excel 
+library(dplyr)    # Simplifying data wrangling 
+library(rvest)    # Reading data from the web 
+library(stringr)  # Easier wrangling of character strings 
+library(here)     # Simplifying working with file paths 
 
 
 ## Set Paths 
 STAT19_LINK <- "https://asmc.com.au/industry-overview/statistics/weekly-crush-statistics-2019/" 
 STAT18_LINK <- "https://asmc.com.au/industry-overview/2018-weekly-crushing-statistics/" 
+
+
+## Create Output Path (if necessary) 
+if(dir.exists("supplementary_data") == FALSE) {
+  dir.create("supplementary_data")
+} 
 
 
 # ------------------------------------------------------------------------------
@@ -92,7 +111,64 @@ crush18 %<>%
 
 
 # ------------------------------------------------------------------------------
+## Prepare to write to Excel ---- 
+crush_wb <- createWorkbook() # create the Excel Workbook 
+modifyBaseFont(crush_wb, fontSize = 8, fontName = "Arial") # Set the formatting 
+
+## Create the tabs for each year 
+addWorksheet(wb = crush_wb, 
+             sheetName = "2018 Crush Statistics", 
+             tabColour = "#ADFF2F") 
+addWorksheet(wb = crush_wb, 
+             sheetName = "2019 Crush Statistics", 
+             tabColour = "#9ACD32") 
+
+## Add formatting for the sheets 
+setColWidths(wb = crush_wb, 
+             sheet = "2018 Crush Statistics", 
+             cols = 1:ncol(crush18), 
+             widths = "auto") 
+setColWidths(wb = crush_wb, 
+             sheet = "2019 Crush Statistics", 
+             cols = 1:ncol(crush19), 
+             widths = "auto") 
+addFilter(wb = crush_wb, 
+          sheet = "2018 Crush Statistics", 
+          rows = 1, 
+          cols = 1:length(crush18)) 
+addFilter(wb = crush_wb, 
+          sheet = "2019 Crush Statistics", 
+          rows = 1, 
+          cols = 1:length(crush19)) 
+
+## Write the data to the tabs 
+writeData(wb = crush_wb, 
+          sheet = "2018 Crush Statistics", 
+          x = crush18, 
+          headerStyle = createStyle(textDecoration = "bold")) 
+writeData(wb = crush_wb, 
+          sheet = "2019 Crush Statistics", 
+          x = crush19, 
+          headerStyle = createStyle(textDecoration = "bold")) 
+
+
+# ------------------------------------------------------------------------------
+## Output the Data to Excel ---- 
+saveWorkbook(crush_wb, 
+             here("supplementary_data", "Industry Crush Statistics.xlsx"), 
+             overwrite = TRUE) 
+
+
+# ------------------------------------------------------------------------------
+## Output in CSV and RDS formats ---- 
+write_csv(crush18, here("supplementary_data", "Industry Crush Stats (2018).csv")) 
+write_csv(crush19, here("supplementary_data", "Industry Crush Stats (2019).csv")) 
+
+saveRDS(crush18, here("supplementary_data", "Industry Crush Stats (2018).rds")) 
+saveRDS(crush19, here("supplementary_data", "Industry Crush Stats (2019).rds")) 
+
 
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
+
